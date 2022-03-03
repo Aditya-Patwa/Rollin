9f88f0ad51bad1465a1dd5921db3bec31a7a1187
AdityaPatwa <patwasmart@gmail.com>
2022-03-03 12:01:58 +0530

    Improving Enemy Performance

--- src/game/enemy.js
+++ src/game/enemy.js
@@ -1,47 +1,76 @@
 let enemies = [];
+let enemyPositions = [
+  {
+    initialPosition: [-180, -21.5, 0],
+    finalPosition: -260
+  }
+];
 
-const createEnemyMesh = () => {
-  let enemy = new BABYLON.MeshBuilder.CreatePlane('enemy', {height: 7, width: 7, sideOrientation: BABYLON.Mesh.DOUBLESIDE});
-  let enemyMat = new BABYLON.StandardMaterial('enemyMat');
-  enemyMat.diffuseTexture = new BABYLON.Texture('./media/enemy.png');
-  
-  enemy.material = enemyMat;
-
-  return enemy;
-};
 
 const createEnemy = (scene) => {
-  let Enemy1 = createEnemyMesh();
-  
-  Enemy1.position = new BABYLON.Vector3(-25, -21.5, 0);
+  let enemy = new BABYLON.MeshBuilder.CreatePlane('enemy', { height: 7, width: 7, sideOrientation: BABYLON.Mesh.DOUBLESIDE });
+  let enemyMat = new BABYLON.StandardMaterial('enemyMat');
+  enemyMat.diffuseTexture = new BABYLON.Texture('./media/enemy.png');
+  enemy.material = enemyMat;
 
-  enemies.push(Enemy1);
-  
+  enemy.position = new BABYLON.Vector3(-25, -21.5, 0);
+
+  enemies.push(enemy);
+
   let enemyAnimation = new BABYLON.Animation('enemyAnimation', 'position.x', 40, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
-  
+
   let animationKeys = [];
-  
+
   animationKeys.push({
     frame: 0,
     value: -25
   });
-  
+
   animationKeys.push({
-    frame: 250/2,
+    frame: 250 / 2,
     value: -70
   });
-  
+
   animationKeys.push({
     frame: 250,
     value: -25
   });
-  
+
   enemyAnimation.setKeys(animationKeys);
-  
-  Enemy1.animations = [];
-  Enemy1.animations.push(enemyAnimation);
-  
-  scene.beginAnimation(Enemy1, 0, 250, true);
-  
-  // scene.beginAnimation(Enemy1, 0, 250, true);
+
+  enemy.animations = [];
+  enemy.animations.push(enemyAnimation);
+
+  enemyPositions.forEach(en => {
+    enemy = enemy.clone();
+    enemy.position = new BABYLON.Vector3(en.initialPosition[0], en.initialPosition[1], en.initialPosition[2]);
+    enemies.push(enemy);
+    let enemyAnimation = new BABYLON.Animation('enemyAnimation', 'position.x', 40, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+    let animationKeys = [];
+
+    animationKeys.push({
+      frame: 0,
+      value: en.initialPosition[0]
+    });
+
+    animationKeys.push({
+      frame: 250 / 2,
+      value: en.finalPosition
+    });
+
+    animationKeys.push({
+      frame: 250,
+      value: en.initialPosition[0]
+    });
+
+    enemyAnimation.setKeys(animationKeys);
+
+    enemy.animations = [];
+    enemy.animations.push(enemyAnimation);
+  });
+
+  enemies.forEach(element => {
+    scene.beginAnimation(element, 0, 250, true);
+  });
 };
